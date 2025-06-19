d1fcd4bc9e0b432642b374d95cc4314df5332621
MisterShoxjahon <sobirzanovsohzahon@gmail.com>
2025-06-19 18:39:29 +0500

    course

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Course from './Pages/Course'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Course />
     </>
   )
 }
---
+++ src/Pages/Course.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Course = () => {
+  return (
+    <section className="container">
+      <div className="mt-[60px]">
+        <div className="flex justify-center">
+          <h1 className="font-bold text-[40px] text-black uppercase">
+            bu kurs siz uchun, agar:
+          </h1>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default Course;
